a5a32f8c497cc09702e93660f10bc9859216a517
홍종훈 <whdgnszz1@snu.ac.kr>
2024-09-09 09:52:18 +0900

    Tailwind, Shadcn 세팅 완료

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+function Home() {
+  return (
+    <>
+      <div className="font-bold">Home</div>
+      <Button className="bg-blue-300">Button</Button>
+    </>
+  );
+}
+
+export default Home;
---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [
+    react(),
+    visualizer({
+      filename: './dist/stats.html',
+      open: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+});
